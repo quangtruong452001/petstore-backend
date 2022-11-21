83276f2967f905f24ebda7d61c145ad2c90d7dfa
hoainambrvt2001 <hoainambrvt2001@gmail.com>
2022-11-21 23:01:54 +0700

    update

--- src/order/order.service.ts
+++ src/order/order.service.ts
@@ -104,27 +104,8 @@ export class OrderService {
       const order = await this.orderModel.findOne(filters);
       let orderDetail = await order.populate('payment');
 
-      // ** Get shipping detail:
-      const config = {
-        headers: {
-          'Content-Type': 'application/json',
-          Token: '5afa38c1-5c4b-11ed-b8cc-a20ef301dcd7',
-        },
-      };
-      const data_raw = {
-        order_code: orderDetail.shipping,
-      };
-      const { data } = await firstValueFrom(
-        this.httpService.post(
-          'https://dev-online-gateway.ghn.vn/shiip/public-api/v2/shipping-order/detail',
-          data_raw,
-          config,
-        ),
-      );
-
       return {
         order: orderDetail,
-        shipping: data.data,
         statusCode: 200,
       };
     } catch (err) {
@@ -137,12 +118,10 @@ export class OrderService {
       // Other information added to order:
       const otherInfos: any = {
         user: userId,
-        confirmStatus: ORDER_STATUS.PENDING,
+        status: ORDER_STATUS.PENDING,
       };
       if (orderDto.payment)
         otherInfos.payment = orderDto.payment;
-      if (orderDto.shipping)
-        otherInfos.shipping = orderDto.shipping;
 
       // Create order in the database:
       const createdOrder = await this.orderModel.create({
@@ -152,9 +131,6 @@ export class OrderService {
 
       return {
         paymentId: orderDto.payment ? orderDto.payment : '',
-        shippingId: orderDto.shipping
-          ? orderDto.shipping
-          : '',
         orderId: createdOrder._id,
         statusCode: 201,
       };
@@ -172,7 +148,7 @@ export class OrderService {
       const updatedOrder = await this.orderModel.updateOne(
         {
           _id: orderId,
-          user: userId,
+          // user: userId,
         },
         {
           ...orderDto,
@@ -199,6 +175,30 @@ export class OrderService {
       };
     } catch (err) {
       throw err;
+    }
+  }
+
+  async count(options: any) {
+    try {
+      options ? options : {};
+      return this.orderModel.count(options);
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
+  }
+
+  async totalSale() {
+    try {
+      let totalSale = 0;
+      const orderList = await this.orderModel.find({});
+      for (let i = 0; i < orderList.length; i++) {
+        totalSale = orderList[i].totalPrice;
+      }
+      return totalSale;
+    } catch (error) {
+      console.log(error);
+      throw error;
     }
   }
 }
--- src/schemas/order.schema.ts
+++ src/schemas/order.schema.ts
@@ -21,12 +21,6 @@ export class Order {
   @Prop({ required: true, type: BillSchema })
   bill: Bill;
 
-  @Prop({ required: true })
-  totalPrice: number;
-
-  @Prop({ required: true })
-  shippingFee: number;
-
   @Prop({
     required: true,
     type: mongoose.Schema.Types.ObjectId,
@@ -39,15 +33,18 @@ export class Order {
     ref: 'Payment',
   })
   payment: string;
-  // 6372f95915aabf9113f9a6e9
-  // 6372feda15aabf9113f9a992
-  // 6374a2b10a1a169d80f044d2
-
-  @Prop()
-  shipping: string;
 
   @Prop({ required: true })
-  confirmStatus: string;
+  shippingTime: string;
+
+  @Prop({ required: true })
+  shippingFee: number;
+
+  @Prop({ required: true })
+  totalPrice: number;
+
+  @Prop({ required: true })
+  status: string;
 }
 
 export const OrderSchema =
