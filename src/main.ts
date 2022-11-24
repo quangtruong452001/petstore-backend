6fbb6227c7bf2744dccbf3d0c1b58bd23b8201b4
hoainambrvt2001 <hoainambrvt2001@gmail.com>
2022-11-24 13:31:38 +0700

    update

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: /.*/,
-    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+    credentials: true,
   });
   // ** NestJS Pipe: https://docs.nestjs.com/pipes to validate data in the dto object.
   app.useGlobalPipes(
